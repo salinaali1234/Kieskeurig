52c893f996dad0c0288cc7819ec43031dc20d4f7
afnan <elhelkaafnan@gmail.com>
2025-03-27 09:33:35 +0100

    Feat - In de XMLController heb ik twee endpoints aangemaakt voor het ophalen en aanvragen van de data.

--- Kieskeurig/src/main/java/nl/hva/kieskeurig/controller/XMLController.java
+++ Kieskeurig/src/main/java/nl/hva/kieskeurig/controller/XMLController.java
@@ -2,24 +2,35 @@ package nl.hva.kieskeurig.controller;
 
 import nl.hva.kieskeurig.model.Candidate;
 import nl.hva.kieskeurig.repository.XMLRepo;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import nl.hva.kieskeurig.service.VotesService;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/xml")
 public class XMLController {
     private final XMLRepo repo;
+    private final VotesService votesService;
 
-    public XMLController(XMLRepo repo) {
+    public XMLController(XMLRepo repo, VotesService votesService) {
         this.repo = repo;
+        this.votesService = votesService;
     }
 
     @GetMapping("/candidates/{partyId}")
     public List<Candidate> getCandidatesOfParty(@PathVariable int partyId) {
         return repo.getCandidatesOfParty(partyId);
+    }
+
+    @PostMapping("/read/{fileName}")
+    public boolean getVotes(@PathVariable String fileName) {
+        return votesService.readResults("C:\\Users\\safae\\IdeaProjects\\ciijeegoomii63\\Kieskeurig\\src\\main\\directory\\Verkiezingsuitslag Tweede Kamer 2023\\Totaaltelling_TK2023.eml.xml");
+    }
+
+    @GetMapping("/votes/parties")
+    public Map<String, Integer> getVotesPerParty() {
+        return votesService.getVotesPerParty();
     }
 }
