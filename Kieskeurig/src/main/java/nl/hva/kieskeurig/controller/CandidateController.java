867831b0db410144e93b55ebb289820e75767ab3
Chibu68 <62471871+Chibu68@users.noreply.github.com>
2025-04-01 14:16:37 +0200

    style: Added JavaDoc to CandidateController

--- Kieskeurig/src/main/java/nl/hva/kieskeurig/controller/CandidateController.java
+++ Kieskeurig/src/main/java/nl/hva/kieskeurig/controller/CandidateController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * API handling everything related to candidates.
+ */
 @RestController
 @RequestMapping("/api/candidates")
 public class CandidateController {
