88aa73c4163c0f69a2d0dd296590117e74ef5789
Chibu68 <62471871+Chibu68@users.noreply.github.com>
2025-04-01 14:19:43 +0200

    style: Added JavaDoc to CandidateRepo

--- Kieskeurig/src/main/java/nl/hva/kieskeurig/repository/CandidateRepo.java
+++ Kieskeurig/src/main/java/nl/hva/kieskeurig/repository/CandidateRepo.java
@@ -2,11 +2,15 @@ package nl.hva.kieskeurig.repository;
 
 import lombok.Getter;
 import nl.hva.kieskeurig.model.Candidate;
+import nl.hva.kieskeurig.service.CandidateService;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Stores the data of {@link CandidateService}
+ */
 @Getter
 @Repository
 public class CandidateRepo {
