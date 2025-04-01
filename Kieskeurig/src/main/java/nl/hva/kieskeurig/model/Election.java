7646f4de98b1cf8a5c55c377d403459990c13a09
Salina Muhammad <salina.muhammad@hva.nl>
2025-04-01 12:13:03 +0200

    fix: getter is placed better

--- Kieskeurig/src/main/java/nl/hva/kieskeurig/model/Election.java
+++ Kieskeurig/src/main/java/nl/hva/kieskeurig/model/Election.java
@@ -1,13 +1,16 @@
 package nl.hva.kieskeurig.model;
 
+import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@Setter
 public class Election {
-    @Setter
     private String electionDate;
+
     private List<Party> parties = new ArrayList<>();
 
     public Election(String electionDate) {this.electionDate = electionDate;}
@@ -28,6 +31,4 @@ public class Election {
     public String toString() {
         return "Election: %s - #of parties: %d".formatted(electionDate, parties.size());
     }
-
-
 }
