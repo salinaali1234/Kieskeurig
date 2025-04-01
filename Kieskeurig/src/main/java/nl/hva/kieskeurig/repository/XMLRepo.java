9537baa38e2a722efcc5c7d4b9f00b5b64f9e419
Salina Muhammad <salina.muhammad@hva.nl>
2025-04-01 12:11:05 +0200

    feat: added a XMLRepo

--- Kieskeurig/src/main/java/nl/hva/kieskeurig/repository/XMLRepo.java
+++ Kieskeurig/src/main/java/nl/hva/kieskeurig/repository/XMLRepo.java
@@ -1,6 +1,8 @@
 package nl.hva.kieskeurig.repository;
 
+//import nl.hva.ict.se.sm3.reader.DutchElectionReader;
 import nl.hva.kieskeurig.model.Candidate;
+import nl.hva.kieskeurig.model.Election;
 import nl.hva.kieskeurig.model.Party;
 import org.springframework.stereotype.Repository;
 
@@ -10,10 +12,14 @@ import java.util.List;
 @Repository
 public class XMLRepo {
 
+//    public void add(Election election) {
+//        election.add(election);
+//    }
+
     public List<Candidate> getCandidatesOfParty(int partyId) {
         // Test
         List<Candidate> candidates = new ArrayList<>();
-        Candidate testCandidate = new Candidate(1, "D.", "Dilan", "Yeşilgöz", "female", "Amsterdam");
+        Candidate testCandidate = new Candidate(1,1, "D.", "Dilan", "Yeşilgöz", "female", "Amsterdam");
 
         candidates.add(testCandidate);
         return candidates;
@@ -27,4 +33,13 @@ public class XMLRepo {
         parties.add(testParty);
         return parties;
     }
+
+    public List<Election> getAll() {
+        return List.of();
+    }
+
+//    public boolean readResults(String folderName) {
+//        DutchElectionReader reader = new DutchElectionReader(this); // Correcte manier om een instantie van DutchElectionReader aan te maken
+//        return reader.readResults(folderName); // Alleen folderName doorgeven, zonder 'party'
+//    }
 }
