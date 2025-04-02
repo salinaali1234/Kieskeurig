9409e1bae295da382c94b4f4a214abf7ae793813
Chibu68 <62471871+Chibu68@users.noreply.github.com>
2025-04-02 23:02:24 +0200

    feat: Made a candidate type for the front-end

---
+++ front-end/src/models/Candidate.ts
@@ -0,0 +1,15 @@
+export type Candidate = {
+    candidateIdentifier: number;
+    initials: string;
+    firstName: string;
+    namePrefix: string;
+    lastName: string;
+    affiliationIdentifier: number;
+    registeredName: string;
+    contestIdentifier: number;
+    contestName: string;
+    electionIdentifier: string;
+    electionCategory: string;
+    electionName: string;
+    electionDate: string;
+}
