b004b289a2dc4642990dca550c52c935d75e11e3
afnan <elhelkaafnan@gmail.com>
2025-04-03 06:48:47 +0200

    Feat - In the index.ts I made a new route to the statistieken page for the table with the national votes.

--- front-end/src/router/index.ts
+++ front-end/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import VoteView from "@/views/VoteView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,12 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    {
+      path: '/statistieken',
+      name: 'Vote',
+      component: VoteView,
+
+    }
 
   ],
 })
