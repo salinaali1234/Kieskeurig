bef7fa1bb3608da43b99fa88efebe0afe14c3682
afnan <elhelkaafnan@gmail.com>
2025-04-03 06:34:25 +0200

    Fix - I made a new controller for the votes and I removed the post request.

--- Kieskeurig/src/main/java/nl/hva/kieskeurig/controller/XMLController.java
+++ Kieskeurig/src/main/java/nl/hva/kieskeurig/controller/XMLController.java
@@ -2,21 +2,18 @@ package nl.hva.kieskeurig.controller;
 
 import nl.hva.kieskeurig.model.Candidate;
 import nl.hva.kieskeurig.repository.XMLRepo;
-import nl.hva.kieskeurig.service.VotesService;
+import nl.hva.kieskeurig.service.VoteService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/xml")
 public class XMLController {
     private final XMLRepo repo;
-    private final VotesService votesService;
 
-    public XMLController(XMLRepo repo, VotesService votesService) {
+    public XMLController(XMLRepo repo, VoteService voteService) {
         this.repo = repo;
-        this.votesService = votesService;
     }
 
     @GetMapping("/candidates/{partyId}")
@@ -24,13 +21,4 @@ public class XMLController {
         return repo.getCandidatesOfParty(partyId);
     }
 
-    @PostMapping("/read/{fileName}")
-    public boolean getVotes(@PathVariable String fileName) {
-        return votesService.readResults("C:\\Users\\safae\\IdeaProjects\\ciijeegoomii63\\Kieskeurig\\src\\main\\directory\\Verkiezingsuitslag Tweede Kamer 2023\\Totaaltelling_TK2023.eml.xml");
-    }
-
-    @GetMapping("/votes/parties")
-    public Map<String, Integer> getVotesPerParty() {
-        return votesService.getVotesPerParty();
-    }
 }
