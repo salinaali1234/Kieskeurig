0c8ca5e32d4d4cdd188e5eb910c7965a22d5bd1e
Esther <Sneeuwuil06@gmail.com>
2025-04-23 23:23:07 +0200

    fix: show the municipalities in a dropdown menu

--- front-end/src/views/Consistuency.vue
+++ front-end/src/views/Consistuency.vue
@@ -33,24 +33,71 @@ onMounted(async () => {
   }
 });
 
-const showConstituency = (id: number) => {
-  router.push(`/Constituency/${id}`)
-}
+const selectedView = ref("");
 </script>
 
 <template>
   <div>
+    <h1 class="page-title">Kieskring {{constituencyId}}</h1>
 
     <table class="data-table">
       <tbody>
-      <tr v-for="municipality in municipalities"
-          :key="municipality[0]"
-          @click="showConstituency(municipality[1])">
-        <td>{{ municipality[0] }}</td>
+      <tr class="dropdown-wrapper">
+        <select v-model="selectedView" class="dropdown">
+          <option disabled value="">Gemeentes</option>
+          <option v-for="municipality in municipalities" value="national">{{municipality[0]}}</option>
 
+        </select>
+        <span class="dropdown-icon">˅</span>
       </tr>
       </tbody>
     </table>
     <p v-if="isVisible && municipalities.length === 0">all gemeentes binnen deze kieskring inladen...</p>
   </div>
 </template>
+
+<style scoped>
+
+.page-title {
+  text-align: center;
+  font-size: 2rem;
+  margin-top: 40px;
+  margin-bottom: 20px;
+  font-weight: bold;
+  color: white;
+}
+
+
+.dropdown-wrapper {
+  position: relative;
+  width: 80%;
+  max-width: 600px;
+  margin: 20px auto;
+}
+
+.dropdown {
+  width: 100%;
+  padding: 12px;
+  font-size: 1rem;
+  border-radius: 8px;
+  border: 1px solid var(--secondary-clr);
+  background-color: #DEBFE9;
+  color: black;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+  appearance: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  cursor: pointer;
+}
+
+.dropdown-icon {
+  position: absolute;
+  right: 16px;
+  top: 50%;
+  transform: translateY(-50%);
+  font-size: 1.2rem;
+  color: black; /* zwarte pijl */
+  pointer-events: none;
+}
+
+</style>
