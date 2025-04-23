770f6f613c3d93f31131451c4bfabf5364a95d3b
afnan <elhelkaafnan@gmail.com>
2025-04-23 19:10:52 +0200

    Fix - Fix arrow to the right format

--- front-end/src/views/VoteView.vue
+++ front-end/src/views/VoteView.vue
@@ -35,7 +35,9 @@ const selectedYear = ref("2023");
         <option value="2023">2023</option>
         <option value="2021">2021</option>
       </select>
-      <span class="dropdown-icon">˅</span>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-down h-4 w-4 opacity-50 dropdown-icon" aria-hidden="true">
+        <path d="m6 9 6 6 6-6"></path>
+      </svg>
     </div>
 
 
