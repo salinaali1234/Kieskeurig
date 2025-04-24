36402a14bade33a5be210712ee575ac783163fb3
Esther <Sneeuwuil06@gmail.com>
2025-04-24 07:35:16 +0200

    feat: sorting constituencies a to z, z to a

--- front-end/src/views/Constituencies.vue
+++ front-end/src/views/Constituencies.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {ref, onMounted, type Ref} from "vue";
+import {ref, onMounted, type Ref, computed} from "vue";
 import "../assets/tableStyle.css"
 import Constituencies from "@/views/Constituencies.vue";
 import NationalVotes from "@/views/NationalVotes.vue";
@@ -12,6 +12,9 @@ interface Constituency {
 
 const constituencies: Ref<any[]> = ref([]);
 const isVisible = ref(false);
+const showSorted = ref(false);
+const sortDirection = ref<'asc' | 'desc' | null>(null); // asc = A-Z, desc = Z-A, null = original
+
 const backendUrl = import.meta.env.VITE_APP_BACKEND_URL;
 const url = `${backendUrl}/api/constituencies/all/Constituencies/0`;
 
@@ -39,15 +42,50 @@ const showConstituency = (id: number) => {
   router.push(`/Constituency/${id}`)
 }
 
+const displayedConstituencies = computed(() => {
+  if (sortDirection.value === 'asc') {
+    return [...constituencies.value].sort((a, b) =>
+      a[0].localeCompare(b[0])
+    );
+  }
+  if (sortDirection.value === 'desc') {
+    return [...constituencies.value].sort((a, b) =>
+      b[0].localeCompare(a[0])
+    );
+  }
+  return constituencies.value;
+});
+
+function toggleSortByName() {
+  if (sortDirection.value === null) {
+    sortDirection.value = 'asc';
+  } else if (sortDirection.value === 'asc') {
+    sortDirection.value = 'desc';
+  } else {
+    sortDirection.value = null;
+  }
+}
+
 
 </script>
 
 <template>
   <div>
 
+    <thead>
+    <tr>
+      <th @click="toggleSortByName()" style="cursor: pointer;" class="sortingButton">
+        sorteren:
+        <span v-if="sortDirection == null">A-Z▼ ▲</span>
+        <span v-if="sortDirection === 'asc'">Z-A ▲ ▼</span>
+        <span v-else-if="sortDirection === 'desc'">origineel</span>
+      </th>
+    </tr>
+    </thead>
+
     <table class="data-table">
       <tbody>
-      <tr v-for="constituency in constituencies"
+      <tr v-for="constituency in displayedConstituencies"
           :key="constituency[0]"
 
           @click="showConstituency(constituency[1])">
@@ -59,3 +97,10 @@ const showConstituency = (id: number) => {
     <p v-if="isVisible && constituencies.length === 0">Geen kieskringen gevonden...</p>
   </div>
 </template>
+
+<style>
+.sortingButton{
+  margin-left: 40px;
+}
+
+</style>
