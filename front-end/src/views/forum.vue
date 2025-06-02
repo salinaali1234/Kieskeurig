ce08f44da527ae158f49ac8450701f522cbb0588
Esther <Sneeuwuil06@gmail.com>
2025-06-02 10:54:44 +0200

    feat: basic frame for the forum page

--- front-end/src/views/forum.vue
+++ front-end/src/views/forum.vue
@@ -1,6 +1,31 @@
+<script>
+
+
+</script>
+
 <template>
-  <l>
-    hello there
-  </l>
+  <div>
+    <h2>Forum</h2>
+    <br>
+    <l>
+      Welkom op de forum pagina van Kieskeurig. <br>
+      Heb je een vraag stel die vooral, of beantwoord vragen :D
+    </l>
+  </div>
+  <div>
+    <h3>
+      Gesprekken
+    </h3>
+  </div>
+  <div>
+    <input>
+    <button>post</button>
+  </div>
 
 </template>
+<style>
+div{
+  margin-top: 20px;
+}
+
+</style>
