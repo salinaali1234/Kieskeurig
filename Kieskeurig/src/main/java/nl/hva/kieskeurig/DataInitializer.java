ef8e1aebbd4149ddbc94c6eedd0c756eced2e701
Chibu68 <62471871+Chibu68@users.noreply.github.com>
2025-06-09 23:39:40 +0200

    Feat: Make sure that DataInitializer doesn't run when unit testing

--- Kieskeurig/src/main/java/nl/hva/kieskeurig/DataInitializer.java
+++ Kieskeurig/src/main/java/nl/hva/kieskeurig/DataInitializer.java
@@ -1,18 +1,30 @@
 package nl.hva.kieskeurig;
 
-import lombok.AllArgsConstructor;
 import nl.hva.kieskeurig.service.ConstituencyService;
 import nl.hva.kieskeurig.service.MunicipalityService;
 import nl.hva.kieskeurig.service.ProvinceService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import javax.xml.stream.XMLStreamException;
+import java.io.IOException;
+
 @Component
-@AllArgsConstructor
 public class DataInitializer implements CommandLineRunner {
     private final ProvinceService provinceService;
     private final ConstituencyService constituencyService;
     private final MunicipalityService municipalityService;
+
+    // Makes sure that DataInitializer doesn't run when unit testing
+    @Value("${app.populate-database-on-startup:true}")
+    private boolean populateDatabaseOnStartup;
+
+    public DataInitializer(ProvinceService provinceService, ConstituencyService constituencyService, MunicipalityService municipalityService) {
+        this.provinceService = provinceService;
+        this.constituencyService = constituencyService;
+        this.municipalityService = municipalityService;
+    }
 
     /**
      * Runs when the back-end starts
@@ -21,6 +33,12 @@ public class DataInitializer implements CommandLineRunner {
      */
     @Override
     public void run(String... args) throws Exception {
+        if (populateDatabaseOnStartup) {
+            initializeData();
+        }
+    }
+
+    private void initializeData() throws XMLStreamException, IOException {
         System.out.println("Initializing data...");
         System.out.println("=========================");
 
@@ -28,21 +46,21 @@ public class DataInitializer implements CommandLineRunner {
             System.out.println("Initializing province data...");
             provinceService.populateDatabase();
             System.out.println("Initializing province data complete.");
-        System.out.println("=========================");
+            System.out.println("=========================");
         }
 
         if (constituencyService.isEmpty()) {
             System.out.println("Initializing constituency data...");
             constituencyService.connectElectionDefinition();
             System.out.println("Initializing constituency data complete.");
-        System.out.println("=========================");
+            System.out.println("=========================");
         }
 
         if (municipalityService.isEmpty()) {
             System.out.println("Initializing municipality data...");
             municipalityService.connectElectionDefinition();
             System.out.println("Initializing municipality data complete.");
-        System.out.println("=========================");
+            System.out.println("=========================");
         }
 
         System.out.println("Initialization complete.");
