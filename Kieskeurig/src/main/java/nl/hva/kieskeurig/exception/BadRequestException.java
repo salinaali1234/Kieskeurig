0e221639215e46242cbd90ad081dc674f2a6b17a
Esther <Sneeuwuil06@gmail.com>
2025-06-12 09:47:38 +0200

    Feat: the bad request with an httpStatus

--- Kieskeurig/src/main/java/nl/hva/kieskeurig/exception/BadRequestException.java
+++ Kieskeurig/src/main/java/nl/hva/kieskeurig/exception/BadRequestException.java
@@ -1,5 +1,10 @@
 package nl.hva.kieskeurig.exception;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class BadRequestException extends RuntimeException {
     public BadRequestException(String message) {
         super(message);
